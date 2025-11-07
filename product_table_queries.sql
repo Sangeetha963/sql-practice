f9f53e3c0240eac01ec5819705a11f054ebc26ad
Sangeetha963 <sangeetharekha99503@gmail.com>
2025-11-07 16:37:51 +0530

    Add product table SQL practice queries

--- product_table_queries.sql
+++ product_table_queries.sql
@@ -50,6 +50,53 @@ SELECT name, price FROM products ORDER BY price DESC LIMIT 1;
 
 
 
+SELECT name, price, category FROM products ORDER BY price ASC LIMIT 1;
+
+SELECT category, COUNT(*) AS cat_count FROM products GROUP BY category; 
+
+SELECT AVG(price) AS avg_price FROM products;
+
+SELECT COUNT(*) AS total_count FROM products;
+
+SELECT name, price, category FROM products ORDER BY name ASC;
+
+UPDATE products SET price = '60000' WHERE id = 1;
+
+INSERT INTO products (name, price, category, mfg) VALUES ('Smartwatch', 4500, 'Electronics', '10/10/2023');
+
+SELECT * FROM products WHERE category IN ('Fashion','Electronics') ORDER BY category ASC;
+
+SELECT * FROM products WHERE name LIKE 'B%';
+
+SELECT * FROM products WHERE name LIKE '%h';
+
+SELECT * FROM products WHERE price BETWEEN '1000' AND '10000';
+
+SELECT name, price, (price*0.9) AS disc_price FROM products; 
+
+SELECT name, price, category, (price*0.5) AS disc_price FROM products;
+
+SELECT * FROM products WHERE mfg IS NULL;
+
+SELECT SUM(price) AS tot_price FROM products;
+
+SELECT name, price, category FROM products WHERE price > (SELECT AVG(price) FROM products);
+
+SELECT name, price, category, mfg FROM products ORDER BY mfg DESC LIMIT 1;
+
+UPDATE products
+SET mfg = 
+  CASE
+    WHEN mfg LIKE '%/%' THEN TO_CHAR(TO_DATE(mfg, 'DD/MM/YYYY'), 'YYYY-MM-DD')
+    ELSE mfg
+  END;
+
+  ALTER TABLE products 
+ALTER COLUMN mfg TYPE DATE 
+USING TO_DATE(mfg, 'YYYY-MM-DD');
+
+
+
 
 
 
