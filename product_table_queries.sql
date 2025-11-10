940fc84abbaf9883c08aeca092e82407f02f4d9a
Sangeetha963 <sangeetharekha99503@gmail.com>
2025-11-10 13:20:58 +0530

    createion of suppliers table and queries

--- product_table_queries.sql
+++ product_table_queries.sql
@@ -97,6 +97,64 @@ USING TO_DATE(mfg, 'YYYY-MM-DD');
 
 
 
+SELECT name, price, category, mfg FROM products WHERE mfg > '2023-01-25';
+
+UPDATE products SET price = price * 1.10 WHERE category = 'Home';
+
+UPDATE products SET price = price - 100 WHERE category = 'Books';
+
+SELECT category, mfg, name, AVG(price) AS avg_price, SUM(price) AS tot_price FROM products GROUP BY category, mfg, name;
+
+UPDATE products SET category = 'Apparel' WHERE category = 'Fashion';
+
+ALTER TABLE products ADD COLUMN stock INT;
+
+UPDATE products SET stock = FLOOR(RANDOM() * 50 + 1);
+
+SELECT name, category, price, mfg, stock FROM products WHERE stock < 10;
+
+SELECT SUM(price*stock) AS inventory_sum FROM products;
+
+SELECT name, price, category, mfg FROM products ORDER BY price DESC LIMIT 3;
+
+SELECT name, category, price, mfg FROM products ORDER BY price ASC LIMIT 3;
+
+SELECT name, category, price, mfg FROM products ORDER BY price, category;
+
+CREATE VIEW active_inventory AS SELECT name, price, category, stock FROM products WHERE stock > 0;
+
+SELECT * FROM active_inventory;
+
+SELECT category, COUNT(*) AS num_products FROM products GROUP BY category HAVING COUNT(*) > 2;
+
+SELECT category, COUNT(*) AS num_produts FROM products GROUP BY category HAVING COUNT(*) > 5;
+
+SELECT price, name, category, ROUND(price) AS rounded_price FROM products;
+
+SELECT name, category, price, mfg FROM products ORDER BY mfg ASC LIMIT 2;
+
+CREATE TABLE suppliers (suppier_id SERIAL PRIMARY KEY, supplier_name VARCHAR(100), contact_email VARCHAR(100));
+
+SELECT * FROM suppliers;
+
+INSERT INTO suppliers(supplier_name, contact_email) VALUES ('TechWorld Pvt Ltd', 'contact@techworld.com'),
+('HomeNeeds Ltd', 'sales@homeneeds.com'),
+('BookStore Co', 'info@bookstore.com'),
+('ApparelHub', 'support@apparelhub.com');
+
+INSERT INTO products(name, price, category, mfg, stock) VALUES ('chair', '20000', 'Office', '2024-08-10', 10)
+
+-- 23️⃣ Add supplier_id column to products and set foreign key relation
+ALTER TABLE products ADD COLUMN supplier_id INT;
+
+ALTER TABLE products ADD CONSTRAINT fk_supplier FOREIGN KEY (supplier_id) REFERENCES suppliers(suppier_id); 
+
+
+-- 24️⃣ Assign suppliers to products
+
+UPDATE products SET supplier_id = CASE WHEN category = 'Electronics' THEN 1 WHEN category = 'Home' THEN 2 WHEN category = 'Books' THEN 3 WHEN category = 'Apparel' THEN 4 END;
+
+UPDATE products SET supplier_id = CASE WHEN category = 'Office' THEN 5 END;
 
 
 
